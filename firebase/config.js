6dd0951c9d6e514ea26318b7647b414dfb842ce2
humbertofb <98710153+humbertofb@users.noreply.github.com>
2025-04-27 21:15:37 +0200

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,12 +2,12 @@
 const firebaseConfig = {
     // Aquí debes insertar la configuración de Firebase de tu proyecto
     // Puedes obtener esta información en la consola de Firebase después de crear un proyecto
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBtf--tWrqRVItAGJGQ6hEk81iggP4I-SU",
+    authDomain: "my-humber-project-319815.firebaseapp.com",
+    projectId: "my-humber-project-319815",
+    storageBucket: "my-humber-project-319815.firebasestorage.app",
+    messagingSenderId: "110994378936",
+    appId: "1:110994378936:web:60b20c16ce5f40b47e644d"
 };
 
 // Inicializar Firebase
