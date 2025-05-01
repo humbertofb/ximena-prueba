e41082692287407eee0b8a9d32ebf5a706195da1
humbertofb <98710153+humbertofb@users.noreply.github.com>
2025-05-01 14:20:55 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
             { color: '#D7C5F8', text: 'Día especial' },
             { color: '#FFE9AE', text: 'Sonrisas juntos' }
         ],
-        autoplaySpeed: 10000, // Intervalo de reproducción automática en ms
+        autoplaySpeed: 7000, // Intervalo de reproducción automática en ms
     };
 
     // Función para inicializar la galería
